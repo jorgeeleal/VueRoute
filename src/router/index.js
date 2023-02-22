f7567c136045fe4a0199676600853d7d54577b8a
leal <jorge.leal@cceo.com.mx>
2023-02-22 09:18:07 -0600

    ejemplo: se anadio otra ruta

--- src/router/index.js
+++ src/router/index.js
@@ -16,8 +16,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/other',
+      name: 'other',
+      component: () => import('../views/OtherView.vue')
     }
   ]
 })
 
-export default router
+export default router;
